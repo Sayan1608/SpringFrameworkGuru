5606559257495cfcb3c8f7a520f64e68461dae66
Sayan Sengupta <=>
2023-12-03 13:05:55 +0530

    Commiting the Bare Project Structure of Project: SpringBootDI

---
+++ SpringBootDI/src/test/java/guru/springframework/springbootdi/SpringBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springbootdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
