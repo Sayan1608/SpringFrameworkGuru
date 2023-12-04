da4b96e6832d3269b861224f21174da0ed7a6831
Sayan Sengupta <=>
2023-12-04 23:48:46 +0530

    Optimized Imports for unused Imports

--- SpringBootDI/src/test/java/guru/springframework/springbootdi/controllers/SetterInjectedControllerTest.java
+++ SpringBootDI/src/test/java/guru/springframework/springbootdi/controllers/SetterInjectedControllerTest.java
@@ -1,12 +1,8 @@
 package guru.springframework.springbootdi.controllers;
 
-import guru.springframework.springbootdi.services.GreetingServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SetterInjectedControllerTest {
